f8636bdb41a843abe886f7c0a92c98b5d00a79d3
sasha <sashanikitenko97@mail.ru>
2017-04-04 23:58:18 +0300

    Change settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y2mvac_s8um1hri6px#qaloth%(v574bolzv!4tgg8qk733-!)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alexghost.pythonanywhere.com']
 
 
 # Application definition
